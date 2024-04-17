17bd58c7dd1d6118d49e9693b725243309611aee
bashartech <144168218+bashartech@users.noreply.github.com>
2024-04-18 01:11:13 +0500

    Add files via upload

---
+++ day2/index.js
@@ -0,0 +1,10 @@
+// Question 1
+console.log("\"Albert Einstein once said, \"A Person who never made a mistake never tried anything combining text\"");
+// Question 2
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, \"A Person who never made a mistake never tried anything combining text\"");
+console.log(message);
+// Question 3
+var nam2 = " \t\n Lechetlier \t\n ";
+console.log(nam2);
+console.log(nam2.trim());
---
+++ day2/index.ts
@@ -0,0 +1,18 @@
+// DAY 2
+
+// Question 4
+console.log(
+  `"Albert Einstein once said, "A Person who never made a mistake never tried anything combining text"`
+);
+
+// Question 5
+
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "A Person who never made a mistake never tried anything combining text"`;
+console.log(message);
+
+// Question 6
+
+let nam2 = " \t\n Lechetlier \t\n ";
+console.log(nam2);
+console.log(nam2.trim()); //.trim is used to remove spaces
